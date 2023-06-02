6b5638181b9e861f6f92736f477ce7a6be20673a
gwon11225 <128041750+gwon11225@users.noreply.github.com>
2023-06-02 20:06:19 +0900

    [Silver I] Title: IOIOI, Time: 120 ms, Memory: 114328 KB -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.py
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -5,12 +5,8 @@ word = stdin.readline().rstrip()
 check = 'I' + 'OI'*n
 cnt = 0
 arr = []
-i = 0
-while word_len != i:
+for i in range(word_len):
     if i <= word_len - n*2+1:
         if word[i:i + n*2 + 1] == check:
             cnt += 1
-            i += 2
-            continue
-    i += 1
 print(cnt)
