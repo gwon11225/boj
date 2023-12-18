5d372d9ffcb0e1fe38173e0d6a12bc0e66175edf
gwon11225 <128041750+gwon11225@users.noreply.github.com>
2023-12-18 19:18:51 +0900

    [Bronze II] Title: 단어의 개수, Time: 56 ms, Memory: 39164 KB -BaekjoonHub

--- 백준/Bronze/1152. 단어의 개수/단어의 개수.py
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.py
@@ -1,14 +1,1 @@
-a = input()
-b = a.split()
-c = a.strip()
-count = 0
-for i in b:
-    if i == ' ':
-        count = count + 1
-
-
-
-if count == len(b):
-    print('0')
-else:
-    print(len(b) - count)
+print(len(input().split()))
