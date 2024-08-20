29223b53e2e04086e8a81e40baf5b03aa9b1ae3c
gwon11225 <128041750+gwon11225@users.noreply.github.com>
2024-08-20 16:57:14 +0900

    [Bronze III] Title: 약수 구하기, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2501. 약수 구하기/약수 구하기.py
+++ 백준/Bronze/2501. 약수 구하기/약수 구하기.py
@@ -1,9 +1,11 @@
-a,b = map(int,input().split())
-arr = []
-for i in range(1,a+1):
-    if a%i == 0:
-        arr.append(i)
-try:
-    print(arr[b-1])
-except:
-    print(0)
+count = 0
+
+n, k = map(int, input().split())
+
+for i in range(1, n + 1):
+    if n % i == 0:
+        count += 1
+    if count == k:
+        print(i)
+        exit()
+print(0)
