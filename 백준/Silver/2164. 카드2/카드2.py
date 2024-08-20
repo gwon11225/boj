d9d85d9f3d74f243288d599783617cb58a83fb9a
gwon11225 <128041750+gwon11225@users.noreply.github.com>
2024-08-20 18:14:46 +0900

    [Silver IV] Title: 카드2, Time: 128 ms, Memory: 123048 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.py
+++ 백준/Silver/2164. 카드2/카드2.py
@@ -1,9 +1,9 @@
 from collections import deque
-n = int(input())
 
-arr = deque([str(i) for i in range(1, n + 1)])
+n = int(input())
+arr = deque([i for i in range(1, n + 1)])
 
 while len(arr) != 1:
     arr.popleft()
     arr.append(arr.popleft())
-print(arr[0])
+print(arr[-1])
