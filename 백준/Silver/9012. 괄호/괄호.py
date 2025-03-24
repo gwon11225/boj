ad47eef95939dafd73067985bfc85428261a1e49
gwon11225 <128041750+gwon11225@users.noreply.github.com>
2025-03-24 17:36:25 +0900

    [Silver IV] Title: 괄호, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.py
+++ 백준/Silver/9012. 괄호/괄호.py
@@ -1,17 +1,12 @@
-n = int(input())
-arr = [input() for i in range(n)]
-
-for i in arr:
-    ar = []
-    for j in list(i):
-        if j == ")":
-            if len(ar) != 0 and ar[-1] == "(":
-                ar.pop()
-            else:
-                ar.append(j)
-        else:
-            ar.append(j)
-    if len(ar) == 0:
-        print("YES")
-    else:
-        print("NO")
+from sys import stdin
+n = int(stdin.readline().rstrip())
+for i in range(n):
+    a = []
+    word = stdin.readline().rstrip()
+    for j in word:
+        a.append(j)
+        if len(a) >= 2:
+            if a[-2:] == ['(',')']:
+                a.pop()
+                a.pop()
+    print("YES" if len(a) == 0 else "NO")
