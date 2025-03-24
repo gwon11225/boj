cf7520ece7c578f21efe12caea6e90622fff731e
gwon11225 <128041750+gwon11225@users.noreply.github.com>
2025-03-24 16:58:43 +0900

    [Bronze III] Title: 팰린드롬인지 확인하기, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.py
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.py
@@ -1,23 +1,8 @@
-word = input()
-if len(word) == 1:
-    print(1)
-    exit()
-word_2 = word
-first = 0
-end = len(word) - 1
-for i in range(len(word)//2):
-    check_word = ''
-    for j in range(len(word)):
-        if j == first:
-            check_word += word_2[end]
-        elif j == end:
-            check_word += word_2[first]
-        else:
-            check_word += word_2[j]
-    first += 1
-    end -= 1
-    word_2 = check_word
-if word_2 == word:
-    print(1)
-else:
-    print(0)
+word = list(input())
+reversed_word = list(reversed(word))
+for i in range(len(word)):
+    if word[i] != reversed_word[i]:
+        print(0)
+        exit()
+        
+print(1)
