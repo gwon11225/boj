0df8ead65dfe0cb67ae02b12b023f5a5deb0cbdb
gwon11225 <128041750+gwon11225@users.noreply.github.com>
2025-03-31 17:04:46 +0900

    [Silver IV] Title: 카드2, Time: 264 ms, Memory: 57044 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.py
+++ 백준/Silver/2164. 카드2/카드2.py
@@ -1,9 +1,19 @@
-from collections import deque
-
 n = int(input())
-arr = deque([i for i in range(1, n + 1)])
 
-while len(arr) != 1:
-    arr.popleft()
-    arr.append(arr.popleft())
+head = n - 1
+leaf = 0
+
+def pop(arr):
+    global leaf
+    leaf += 1
+    return arr[leaf - 1]
+
+
+arr = [i for i in range(1, n + 1)]
+
+
+for i in range(n - 1):
+    pop(arr)
+    arr.append(pop(arr))
+    
 print(arr[-1])
