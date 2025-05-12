be4529c7b27403b574999a93af72903850057e7e
gwon11225 <128041750+gwon11225@users.noreply.github.com>
2025-05-12 17:03:18 +0900

    [Bronze II] Title: 블랙잭, Time: 72 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.py
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -1,16 +1,12 @@
-from itertools import *
-
 n, m = map(int, input().split())
-
 arr = list(map(int, input().split()))
 
-answer = sorted(list(permutations(arr, 3)))
+s = 0
 
-real_answer = 0
-
-for i in answer:
-    if sum(i) <= m:
-        if real_answer < sum(i):
-            real_answer = sum(i)
-
-print(real_answer)
+for i in range(n):
+    for j in range(i + 1, n):
+        for k in range(j + 1, n):
+            t = arr[i] + arr[j] + arr[k]
+            if t <= m and t >= s:
+                s = t
+print(s)
